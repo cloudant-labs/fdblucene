e42c6e6a06d97c4c13b5ae3d4f0d563951cc2fbc
Robert Newson <rnewson@apache.org>
2019-06-14 16:04:34 +0100

    remove unused method and class

--- src/main/java/com/cloudant/fdblucene/Utils.java
+++ src/main/java/com/cloudant/fdblucene/Utils.java
@@ -1,22 +1,19 @@
 /*******************************************************************************
  * Copyright 2019 IBM Corporation
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  *******************************************************************************/
 package com.cloudant.fdblucene;
-
-import java.io.IOException;
-import java.util.concurrent.CompletionException;
 
 import org.apache.lucene.util.BytesRef;
 
@@ -31,30 +28,6 @@ class Utils {
             final String str = String.format(format, args);
             txn.options().setDebugTransactionIdentifier(str);
             txn.options().setLogTransaction();
-        }
-    }
-
-    /**
-     * Allow natural throwing of IOException from Lucene methods within an FDB
-     * transaction.
-     *
-     * @throws IOException
-     */
-    static <T> T run(final TransactionContext txc, IOFunction<? super Transaction, T> retryable) throws IOException {
-        try {
-            return txc.run(txn -> {
-                try {
-                    return retryable.apply(txn);
-                } catch (final IOException e) {
-                    throw new CompletionException(e);
-                }
-            });
-        } catch (final CompletionException e) {
-            final Throwable cause = e.getCause();
-            if (cause instanceof IOException) {
-                throw (IOException) cause;
-            }
-            throw e;
         }
     }
 
